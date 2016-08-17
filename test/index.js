c29ce3b5e01ffe2060cb5c6484d70be38795f614
Sebastian Werner <s.werner@sebastian-software.de>
2016-08-17 11:08:50 +0200

    Added asset file for testing

--- test/index.js
+++ test/index.js
@@ -1,8 +1,11 @@
 import classes1 from "./index.css"
-console.log("Imported from CSS:", classes1)
+console.log("Classes from CSS:", classes1)
 
 import classes2 from "./alternate.sss"
-console.log("Imported from SSS:", classes2)
+console.log("Classes from SSS:", classes2)
+
+import url from "./logo.svg"
+console.log("Logo URL:", url)
 
 var elem = document.createElement("div")
 elem.className = classes1.root

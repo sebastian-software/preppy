cb73a3354ab7afdc9cb5b17a0e54ca3f6a2501dc
Sebastian Werner <s.werner@sebastian-software.de>
2016-08-17 08:57:02 +0200

    Worked on more realistic test

--- test/index.js
+++ test/index.js
@@ -1,2 +1,6 @@
-import classes from "./myfile.css"
+import classes from "./index.css"
 console.log("CLASSES: ", classes)
+
+var elem = document.createElement("div")
+elem.className = classes.root
+document.body.appendChild(elem)

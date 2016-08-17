9d2cfa12ef9f03e4751822b4f6da3dedd2c59f2e
Sebastian Werner <s.werner@sebastian-software.de>
2016-08-17 10:23:38 +0200

    Basic postcss processing for stylesheets to resolve imports

--- src/loader.js
+++ src/loader.js
@@ -4,20 +4,60 @@ import crypto from "crypto"
 import denodeify from "denodeify"
 import fse from "fs-extra"
 
+import postcss from "postcss"
+import postcssSmartImport from "postcss-smart-import"
+import postcssParserSugarss from "sugarss"
+import postcssParserScss from "postcss-scss"
+
 var copyAsync = denodeify(fse.copy)
+var writeAsync = denodeify(fse.outputFile)
+
+function isAssetFile(id) {
+  var fileExt = path.extname(id).slice(1)
+  return !(fileExt === "" || (/^(json|jsx|js|es|es5|es6)$/).exec(fileExt))
+}
+
+
+const styleExtensions = {
+  ".css": null,
+  ".sss": postcssParserSugarss,
+  ".scss": postcssParserScss
+}
+
+const postcssPlugins = [
+  postcssSmartImport()
+]
+
+function processStyle(code, id, dest) {
+  return postcss(postcssPlugins)
+    .process(code, { from: id, to: dest }).then((result) => {
+      return writeAsync(dest, result)
+    }).catch(function(err) {
+      console.error(err)
+    })
+}
+
+const staticAST = {
+  type: "Program",
+  sourceType: "module",
+  start: 0,
+  end: null,
+  body: []
+}
+
 
 export default function(outputFolder)
 {
   return {
     name: "file-loader",
-    transform: function(code, id) {
-      var fileExt = path.extname(id).slice(1)
-      if (fileExt === "" || (/^(json|jsx|js|es|es5|es6)$/).exec(fileExt))
+    transform: function(code, id)
+    {
+      if (!isAssetFile(id))
         return null
 
       console.log("Transform: ", id, code)
       return {
-        code: `export default null`,
+        code: `export default "FIXME-PROTECT-IMPORT"`,
         map: { mappings: "" }
       }
     },
@@ -25,12 +65,13 @@ export default function(outputFolder)
 
     load: function(id)
     {
-      var fileExt = path.extname(id).slice(1)
-      if (fileExt === "" || (/^(json|jsx|js|es|es5|es6)$/).exec(fileExt))
+      if (!isAssetFile(id))
         return null
 
       const input = fs.createReadStream(id)
       const hash = crypto.createHash("sha256")
+
+      var fileData = ""
 
       return new Promise((resolve, reject) =>
       {
@@ -39,34 +80,49 @@ export default function(outputFolder)
           var data = input.read()
           if (data)
           {
+            fileData += data
             hash.update(data);
           }
           else
           {
             var fileHasher = crypto.createHash("sha1")
             var fileSource = id
-            var idDest = path.basename(id) + hash.digest("hex").slice(0, 6)
+            var fileExt = path.extname(id)
+            var fileHash = hash.digest("hex").slice(0, 8)
+            var idDest = path.basename(id, fileExt) + "-" + fileHash + fileExt
             var fileDest = path.join(outputFolder, idDest)
 
-            return copyAsync(fileSource, fileDest).then(function()
+
+            if (fileExt in styleExtensions)
             {
-              console.log("Copied:", fileSource, "=>", fileDest)
-              const ast = {
-                type: "Program",
-                sourceType: "module",
-                start: 0,
-                end: null,
-                body: []
-              }
+              console.log("Processing style:", id)
 
-              console.log(`import classes from "./${idDest}"; export default classes;`)
+              return processStyle(fileData, fileSource, fileDest).then(function()
+              {
+                console.log("Processed:", fileSource, "=>", fileDest)
 
-              resolve({
-                ast,
-                code: `import classes from "./${idDest}"; export default classes;`,
-                map: { mappings: "" }
+                resolve({
+                  ast : staticAST,
+                  code: `import h${fileHash} from "./${idDest}"; export default h${fileHash};`,
+                  map: { mappings: "" }
+                })
               })
-            })
+            }
+            else
+            {
+              console.log("Processing asset:", id)
+
+              return copyAsync(fileSource, fileDest).then(function()
+              {
+                console.log("Copied:", fileSource, "=>", fileDest)
+
+                resolve({
+                  ast : staticAST,
+                  code: `import h${fileHash} from "./${idDest}"; export default h${fileHash};`,
+                  map: { mappings: "" }
+                })
+              })
+            }
           }
         })
       })

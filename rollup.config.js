8b3b830c0fb43f1ee958aa0bcf31a2e04d678679
Sebastian Werner <s.werner@sebastian-software.de>
2017-02-14 15:18:58 +0100

    Adjusted executable for prepublish + renamed rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import buble from "rollup-plugin-buble"
 import json from "rollup-plugin-json"
+import executable from "rollup-plugin-executable"
 import builtinModules from "builtin-modules"
 
 var pkg = require('./package.json')
@@ -7,13 +8,14 @@ var external = Object.keys(pkg.dependencies).concat(builtinModules)
 
 export default {
   entry: "src/index.js",
-  dest: "bin/prepublish.js",
+  dest: "bin/prepublish",
   format: "cjs",
   sourceMap: true,
   external: external,
   banner: "#!/usr/bin/env node\n",
   plugins: [
     json(),
-    buble()
+    buble(),
+    executable()
   ]
 }

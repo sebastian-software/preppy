d0a0be74f7b849cb4cc807b10762bb601fd312d9
Nuba Princigalli <nuba@users.noreply.github.com>
2017-08-01 16:16:19 -0300

    quick fix for banner when author is a string

--- src/getBanner.js
+++ src/getBanner.js
@@ -5,7 +5,7 @@ export default function getBanner(pkg) {
     if (typeof pkg.author === "object") {
       banner += ` by ${pkg.author.name} <${pkg.author.email}>`
     } else if (typeof pkg.author === "string") {
-      banner += ` by ${pkg.author.name}`
+      banner += ` by ${pkg.author}`
     }
   }
 

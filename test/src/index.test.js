c7a3f374da120cff07b545ab7978ca99a540ada0
Sebastian Werner <s.werner@sebastian-software.de>
2017-10-09 18:24:28 +0200

    Updated test to deal with changing version strings

--- test/src/index.test.js
+++ test/src/index.test.js
@@ -3,6 +3,10 @@ import { exec } from "child_process"
 import { readFile } from "fs"
 import rimraf from "rimraf"
 
+import pkg from "../../package.json"
+
+const versionString = "simplepublish v" + pkg.version
+
 const lazyExec = promisify(exec)
 const lazyRead = promisify(readFile)
 const lazyDelete = promisify(rimraf)
@@ -14,8 +18,8 @@ test("Publish Test File", async () => {
   const { stdout, stderr } = await lazyExec("node ./bin/simplepublish --input ./test/src/index.js --output-folder ./test/lib")
 
   const cjs = await lazyRead("./test/lib/node.classic.commonjs.js", "utf8")
-  expect(cjs).toMatchSnapshot()
+  expect(cjs.replace(versionString, "VERSION_STRING")).toMatchSnapshot()
 
   const esm = await lazyRead("./test/lib/node.classic.esmodule.js", "utf8")
-  expect(esm).toMatchSnapshot()
+  expect(esm.replace(versionString, "VERSION_STRING")).toMatchSnapshot()
 })

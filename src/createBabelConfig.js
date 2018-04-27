d1c5e1510171be0c1aa39882f2a325070408326d
Sebastian Werner <s.werner@sebastian-software.de>
2018-04-27 11:18:25 +0200

    Tweaked babel setting. Seems that I confused a few things here. I figure following my own docs 'externalHelpers' is the correct flag to enable.

--- src/createBabelConfig.js
+++ src/createBabelConfig.js
@@ -3,8 +3,8 @@ import babel from "rollup-plugin-babel"
 export default function createBabelConfig() {
   return {
     classic: babel({
-      // Rollup Setting: Allow usage of transform-runtime for referencing to a common library of polyfills
-      runtimeHelpers: true,
+      // Rollup Setting: Prefer usage of a common library of helpers
+      externalHelpers: true,
 
       // Do not transpile external code
       // https://github.com/rollup/rollup-plugin-babel/issues/48#issuecomment-211025960

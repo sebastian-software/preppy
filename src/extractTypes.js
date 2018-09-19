f495c9e4a8f237f6d9d9fb721e165c7c426232af
Sebastian Werner <s.werner@sebastian-software.de>
2018-09-19 18:41:32 +0200

    Added typescript helper to extract into type declaration files.

---
+++ src/extractTypes.js
@@ -0,0 +1,40 @@
+import * as ts from "typescript";
+
+// Compiler based on code shown in the official docs:
+// https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API
+function compile(fileNames, options) {
+  const program = ts.createProgram(fileNames, options)
+  const emitResult = program.emit()
+
+  const allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics)
+
+  allDiagnostics.forEach((diagnostic) => {
+    if (diagnostic.file) {
+      const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
+        diagnostic.start
+      )
+      const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n")
+      console.log(
+        `${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`
+      )
+    } else {
+      console.log(`${ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n")}`)
+    }
+  })
+
+  const exitCode = emitResult.emitSkipped ? 1 : 0
+  console.log(`Process exiting with code '${exitCode}'.`)
+  process.exit(exitCode)
+}
+
+export default function extractTypes(fileName, outputFolder) {
+  console.log("Extract types:", fileName, "=>", outputFolder)
+  return compile([fileName], {
+    declarationDir: outputFolder,
+    maxNodeModuleJsDepth: 0,
+    noEmitOnError: false,
+    declaration: true,
+    emitDeclarationOnly: true,
+    target: ts.ScriptTarget.ES5
+  })
+}

55b2b5dc0962b9a4023e7f321698f06733ab96f9
Sebastian Werner <s.werner@sebastian-software.de>
2018-09-20 11:03:38 +0200

    Making execution even smarter by improving caller lookup. Auto formatted file.

--- babel.config.js
+++ babel.config.js
@@ -2,48 +2,47 @@
 module.exports = (api) => {
   const env = api.env()
   const caller = api.caller((inst) => (inst && inst.name) || "any")
-  api.cache(() => env + "::" + caller)
 
-  console.log(">>> Babel Env:", env, "Caller:", caller)
-  console.log(">>> Modules:", env === "test" || caller === "@babel/node" ? "commonjs" : false)
+  const isBundler = caller === "rollup-plugin-babel"
+  const isCli = caller === "@babel/node"
+  const modules = (env === "test" && !isBundler) || isCli ? "commonjs" : false
+
+  console.log(`>>> Babel: Env="${env}" Caller="${caller}" Modules="${modules}"`)
 
   return {
-    "comments": false,
-    "sourceMaps": true,
-    "plugins": [
+    comments: false,
+    sourceMaps: true,
+    plugins: [
       "module:fast-async",
       [
         "@babel/proposal-class-properties",
         {
-          "loose": true
+          loose: true
         }
       ],
       [
         "@babel/proposal-object-rest-spread",
         {
-          "useBuiltIns": true,
-          "loose": true
+          useBuiltIns: true,
+          loose: true
         }
       ],
       [
         "@babel/plugin-transform-runtime",
         {
-          "helpers": true,
-          "regenerator": false
+          helpers: true,
+          regenerator: false
         }
       ]
     ],
-    "presets": [
+    presets: [
       [
         "@babel/env",
         {
-          "exclude": [
-            "transform-regenerator",
-            "transform-async-to-generator"
-          ],
-          "useBuiltIns": "usage",
-          "loose": true,
-          "modules": env === "test" || caller === "@babel/node" ? "commonjs" : false
+          exclude: [ "transform-regenerator", "transform-async-to-generator" ],
+          useBuiltIns: "usage",
+          loose: true,
+          modules
         }
       ],
       [
@@ -51,8 +50,8 @@ module.exports = (api) => {
         {
           // We like JSX everywhere. No reason why we have to deal with
           // legacy type assertion supported in earlier versions.
-          "allExtensions": true,
-          "isTSX": true
+          allExtensions: true,
+          isTSX: true
         }
       ]
     ]

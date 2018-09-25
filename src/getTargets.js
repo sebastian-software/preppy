7288a6f95e00588af8e1244088b93abf16577614
Sebastian Werner <s.werner@sebastian-software.de>
2018-09-25 14:45:11 +0200

    Added new cli targets accepting content from 'cli' folder. Formatting of code

--- src/getTargets.js
+++ src/getTargets.js
@@ -5,20 +5,38 @@ export default function getTargets(command) {
 
   if (command.flags.inputLib) {
     if (!existsSync(command.flags.inputLib)) {
-      throw new Error(`Library entry point specified does not exist: ${command.flags.inputLib}!`)
+      throw new Error(
+        `Library entry point specified does not exist: ${command.flags.inputLib}!`
+      )
     }
     targets.library = command.flags.inputLib
   } else if (!command.flags.inputCli) {
-    targets.library = [ "src/index.js", "src/index.jsx", "src/index.ts", "src/index.tsx" ].filter(existsSync)[0]
+    targets.library = [
+      "src/index.js",
+      "src/index.jsx",
+      "src/index.ts",
+      "src/index.tsx"
+    ].filter(existsSync)[0]
   }
 
   if (command.flags.inputCli) {
     if (!existsSync(command.flags.inputCli)) {
-      throw new Error(`CLI entry point specified does not exist: ${command.flags.inputCli}!`)
+      throw new Error(
+        `CLI entry point specified does not exist: ${command.flags.inputCli}!`
+      )
     }
     targets.binary = command.flags.inputCli
   } else if (!command.flags.inputLib) {
-    targets.binary = [ "src/cli.js", "src/cli.jsx", "src/cli.ts", "src/cli.tsx" ].filter(existsSync)[0]
+    targets.binary = [
+      "src/cli.js",
+      "src/cli.jsx",
+      "src/cli.ts",
+      "src/cli.tsx",
+      "src/cli/index.js",
+      "src/cli/index.jsx",
+      "src/cli/index.ts",
+      "src/cli/index.tsx"
+    ].filter(existsSync)[0]
   }
 
   if (targets.library == null && targets.binary == null) {

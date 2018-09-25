0e6ace4e6cb7b2531c3193540b3e6f67988a9567
Sebastian Werner <s.werner@sebastian-software.de>
2018-09-25 22:31:10 +0200

    Added new browser outputs

--- src/getOutputMatrix.js
+++ src/getOutputMatrix.js
@@ -1,3 +1,4 @@
+/* eslint-disable complexity */
 export default function getOutputMatrix(command, PKG_CONFIG) {
   // Handle special case to generate a binary file based on config in package.json
   const binaryConfig = PKG_CONFIG.bin
@@ -14,7 +15,10 @@ export default function getOutputMatrix(command, PKG_CONFIG) {
     // Library Targets
     main: PKG_CONFIG["main"] || null,
     module: PKG_CONFIG["module"] || PKG_CONFIG["jsnext:main"] || null,
+
+    // Browser Targets
     umd: PKG_CONFIG["umd"] || PKG_CONFIG["unpkg"] || null,
+    browser: PKG_CONFIG["browser"] || null,
 
     // Binary Target
     bin: binaryOutput || null,
@@ -27,7 +31,8 @@ export default function getOutputMatrix(command, PKG_CONFIG) {
   if (outputFolder) {
     matrix.main = `${outputFolder}/index.cjs.js`
     matrix.module = `${outputFolder}/index.esm.js`
-    matrix.umd = `${outputFolder}/index.umd.js`
+    matrix.browser = `${outputFolder}/index.browser.esm.js`
+    matrix.umd = `${outputFolder}/index.browser.umd.js`
     matrix.bin = `${outputFolder}/cli.js`
     matrix.types = `${outputFolder}/index.d.js`
   }

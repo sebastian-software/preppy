88ef9c190006c89f0ef0247e781fe2e0eb5c69b1
Sebastian Werner <s.werner@sebastian-software.de>
2019-01-10 15:07:27 +0100

    Added new "min" test which uses ".min" in filenames to produce minified files in output using Terser.

---
+++ test/esnext-min/index.test.js
@@ -0,0 +1,30 @@
+/* global __dirname */
+import { readFile } from "fs"
+import { resolve } from "path"
+import pify from "pify"
+import rimraf from "rimraf"
+
+import preppy from "../../src/index"
+
+const lazyRead = pify(readFile)
+const lazyDelete = pify(rimraf)
+
+jest.setTimeout(20000)
+
+test("Publish Test File via Babel", async () => {
+  await lazyDelete(resolve(__dirname, "./dist"))
+
+  await preppy({
+    root: __dirname
+  })
+
+  expect(
+    await lazyRead(resolve(__dirname, "dist/index.cjs.min.js"), "utf8")
+  ).toMatchSnapshot("cjs")
+  expect(
+    await lazyRead(resolve(__dirname, "dist/index.esm.min.js"), "utf8")
+  ).toMatchSnapshot("esm")
+  expect(
+    await lazyRead(resolve(__dirname, "dist/index.umd.min.js"), "utf8")
+  ).toMatchSnapshot("umd")
+})

2091ce7d65726b545e7632198c5d81111f8ef1ef
Sebastian Werner <s.werner@sebastian-software.de>
2019-03-29 11:52:34 +0100

    Minor lint fix

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,5 @@
+/* global jest */
+
 // Rewrite console.log/debug to a mock. This shouldn't be required to run in tests
 // and makes test runner output much more calm and focused. We keep warn/error and
 // other not so often used methods intact.

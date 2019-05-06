082091f4dadefca953e9cbac3ebf3ce03bfc14fe
Sebastian Werner <s.werner@sebastian-software.de>
2019-05-06 18:11:57 +0200

    Added new test case for transpiling JSX fully

---
+++ test/jsx-react/index.test.js
@@ -0,0 +1,31 @@
+/* global __dirname */
+import { readFile } from "fs"
+import { resolve } from "path"
+import pify from "pify"
+import rimraf from "rimraf"
+
+import preppy from "../../src/index"
+
+const lazyRead = pify(readFile)
+const lazyDelete = pify(rimraf)
+
+jest.setTimeout(20000)
+
+test("Publish Test File via Babel xx", async () => {
+  await lazyDelete(resolve(__dirname, "./dist"))
+
+  await preppy({
+    root: __dirname,
+    quiet: true
+  })
+
+  expect(
+    await lazyRead(resolve(__dirname, "dist/index.cjs.js"), "utf8")
+  ).toMatchSnapshot("cjs")
+  expect(
+    await lazyRead(resolve(__dirname, "dist/index.esm.js"), "utf8")
+  ).toMatchSnapshot("esm")
+  expect(
+    await lazyRead(resolve(__dirname, "dist/index.umd.js"), "utf8")
+  ).toMatchSnapshot("umd")
+})
---
+++ test/jsx-react/src/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { FormattedMessage } from "react-intl"
+
+export function MyButton() {
+  return (
+    <button>
+      <FormattedMessage id="press-button" />
+    </button>
+  )
+}

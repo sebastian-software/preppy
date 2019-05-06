19dcb80f72f1267c68e72fe20a974776363bac96
Sebastian Werner <s.werner@sebastian-software.de>
2019-05-06 18:21:12 +0200

    Added new test case for transpiling JSX with TS

---
+++ test/typescript-jsx-react/src/types.tsx
@@ -0,0 +1,12 @@
+/**
+ * Values of fields in the form
+ */
+export interface FormValues {
+  [field: string]: any
+}
+
+export const something = 42
+
+export function Item() {
+  return <b>Bold</b>
+}

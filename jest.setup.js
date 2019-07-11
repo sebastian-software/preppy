f798f4361ee2c414b69054af7cb99cf4f6c1e945
Sebastian Werner <s.werner@sebastian-software.de>
2019-07-11 14:51:53 +0200

    Cleanup

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,6 @@
-/* global jest */
-
 // Rewrite console.log/debug to a mock. This shouldn't be required to run in tests
 // and makes test runner output much more calm and focused. We keep warn/error and
 // other not so often used methods intact.
-console.clear = jest.fn()
 console.warn = jest.fn()
 console.log = jest.fn()
 console.debug = jest.fn()
